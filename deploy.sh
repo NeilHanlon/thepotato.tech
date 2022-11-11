a5633fb9facb06de6ceafb4e67120781fd4a08c5
Neil Hanlon <neil@shrug.pw>
2022-11-11 16:14:34 -0500

    CI.. or something

---
+++ deploy.sh
@@ -0,0 +1,23 @@
+#!/bin/bash -ex
+
+REMOTE=$(git remote get-url origin)
+
+rm -fr pages.git
+mkdir pages.git
+
+( cd pages.git && git init -b pages )
+rsync -av public/* pages.git
+
+cat << EOF > pages.git/.domains
+neilhanlon.me
+neilhanlon.com
+hanlon.ninja
+thepotato.tech
+EOF
+
+cd pages.git
+
+git add -A
+git commit -m "Deployment at $(date -u -Is)"
+git remote add origin $REMOTE
+git push -f origin pages
