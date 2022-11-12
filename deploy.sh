702de893b6fcc8e1c746531e4d02fb90496b7c55
Neil Hanlon <neil@shrug.pw>
2022-11-11 22:52:13 -0500

    test

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
 export GIT_AUTHOR_NAME="${GIT_AUTHOR_NAME}"
 export GIT_AUTHOR_EMAIL="${GIT_AUTHOR_EMAIL}"
 
-REMOTE="${CI_REPO_SCM}"
+REMOTE="${CI_REPO_REMOTE}"
 
 rm -fr pages.git
 mkdir pages.git
